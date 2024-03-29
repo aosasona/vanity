f9b082072f047a895ef4c76f72b929cc03bc6b30
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-03-29 23:43:56 +0000

    Added subpath and new project types

--- config/package.go
+++ config/package.go
@@ -12,20 +12,25 @@ type (
 	}
 
 	Package struct {
-		Name   string      `json:"name"`
-		Repo   Repository  `json:"repo"`
-		Type   PackageType `json:"type"`
-		Readme string      `json:"readme"`
+		Name    string      `json:"name"`
+		Repo    Repository  `json:"repo"`
+		Type    PackageType `json:"type"`
+		SubPath string      `json:"subPath"`
+		Readme  string      `json:"readme"`
 	}
 
 	Packages []Package
 )
 
 const (
-	Library    PackageType = "library"
-	Project    PackageType = "project"
+	Module     PackageType = "module"
 	Executable PackageType = "executable"
+	Project    PackageType = "project"
 )
+
+func (p Packages) Len() int           { return len(p) }
+func (p Packages) Less(i, j int) bool { return p[i].Name < p[j].Name }
+func (p Packages) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func (p Packages) Get(name string) (Package, bool) {
 	for _, pkg := range p {
