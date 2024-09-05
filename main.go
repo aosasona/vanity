5f65144a0dc4dc6a140eb1a6de03769d9fa644a7
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-09-05 09:30:20 +0100

    Added support for cmd folder

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 	r.Get("/", web.Index)
 	r.Get("/schemas/{schema}", web.Schemas)
 	r.Get("/{package}", web.ServePackage)
+	r.Get("/{package}/cmd/*", web.ServePackage)
 	r.Get("/{package}/v{version}", web.ServePackage)
 
 	srv := &http.Server{
